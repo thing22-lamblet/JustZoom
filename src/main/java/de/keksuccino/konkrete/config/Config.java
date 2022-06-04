f0e922a57b727b7b28ece577c49c0dfa34f59d9b
Les Matheson <les.matheson@gmail.com>
2022-06-03 21:11:41 -0400

    Add Config.java

---
+++ src/main/java/de/keksuccino/konkrete/config/Config.java
@@ -0,0 +1,9 @@
+package de.keksuccino.konkrete.config;
+
+public class Config {
+    public Config(String filename) {
+
+    };
+    public void registerValue(String name, double value, String subName, String desc) {
+    };
+}
