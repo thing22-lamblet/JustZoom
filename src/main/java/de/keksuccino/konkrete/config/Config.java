32f08cdba025ba66f2d50949d68b249e928fcce0
Les Matheson <les.matheson@gmail.com>
2022-06-03 21:37:12 -0400

    Build is somewhat less broken
    We've stubbed the prototypes for Config class methods

--- src/main/java/de/keksuccino/konkrete/config/Config.java
+++ src/main/java/de/keksuccino/konkrete/config/Config.java
@@ -6,4 +6,13 @@ public class Config {
     };
     public void registerValue(String name, double value, String subName, String desc) {
     };
+    public void registerValue(String name, bool value, String subName, String desc) {
+    };
+    public double getOrDefault(String name, double defValue) {
+        return 0;
+    };
+    public bool getOrDefault(String name, bool defValue) {
+        return false;
+    };
+    public void clearUnusedValues() {};
 }
